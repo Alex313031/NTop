4249098e521ef9874c45545a83e2cb3553468506
Alexander Frick <Alex313031@gmail.com>
2026-07-23 02:13:37 -0500

    update build.bat and CMakeLists.txt to use XP-compatible flags from Makefile

--- build.bat
+++ build.bat
@@ -7,16 +7,22 @@ pushd build
 
 IF "%NTOP_VERSION%"=="" SET NTOP_VERSION=dev
 
+REM Same flags as the Makefile/CMake builds: target XP+, import
+REM GetProcessMemoryInfo from psapi.dll, Unicode build, SSE2 float math.
+REM Remove /DUNICODE /D_UNICODE for an ANSI build.
+SET NTOP_FLAGS=/D_WIN32_WINNT=0x0501 /DWINVER=0x0501 /DPSAPI_VERSION=1 /DUNICODE /D_UNICODE -W4 /GA /MT /arch:SSE2
+SET NTOP_LIBS=Kernel32.lib Advapi32.lib User32.lib Psapi.lib
+
 IF "%~1"=="-release" (
 	REM Release build
   echo NTop Release build
 	rc /nologo /fo ntop.res ..\src\ntop.rc
-	cl /DNTOP_VER="%NTOP_VERSION%" -W4 /GA /MT /O2 ..\src\ntop.c ..\src\util.c ..\src\vi.c ntop.res Advapi32.lib User32.lib
+	cl /DNTOP_VER="%NTOP_VERSION%" %NTOP_FLAGS% /O2 ..\src\ntop.c ..\src\util.c ..\src\vi.c ntop.res %NTOP_LIBS% /link /SUBSYSTEM:CONSOLE,5.01
 ) else (
   REM Debug build
   echo NTop Debug build
   rc /nologo /D_DEBUG /fo ntop.res ..\src\ntop.rc
-  cl /DNTOP_VER=%NTOP_VERSION% -W4 /GA /MT /Z7 ..\src\ntop.c ..\src\util.c ..\src\vi.c ntop.res Advapi32.lib User32.lib
+  cl /DNTOP_VER=%NTOP_VERSION% %NTOP_FLAGS% /Z7 ..\src\ntop.c ..\src\util.c ..\src\vi.c ntop.res %NTOP_LIBS% /link /SUBSYSTEM:CONSOLE,5.01
 )
 
 echo Built NTop version %NTOP_VERSION%!
